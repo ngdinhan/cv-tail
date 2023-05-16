c9b90949ff46ddf8182ad4dfb0f9988d6f33352f
An Nguyen <ngdinhan.209@gmail.com>
2023-05-16 09:24:45 +0700

    add animation section skills

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,4 @@
+// active menu start
 var sections = document.querySelectorAll("section");
 var navLi = document.querySelectorAll("nav .vertical-menu li");
 window.onscroll = () => {
@@ -18,3 +19,66 @@ window.onscroll = () => {
         }
     });
 };
+// active menu end
+
+// menu button start
+$('.menu-icon button').on( 'click', function() {
+    $('header.desktop-header, .main-layout, header.mobile-header').toggleClass('open');
+});
+
+$('main.main-layout').on( 'click', function() {
+    $('header.desktop-header, .main-layout, header.mobile-header').removeClass('open');
+});
+
+$('.vertical-menu li a').on( 'click', function() {
+    $('header.desktop-header, .main-layout, header.mobile-header').removeClass('open');
+});
+
+$('.search-button').on( 'click', function() {
+    $('.search-popup').addClass('open');
+});
+
+$('.search-popup .close').on( 'click', function() {
+    $('.search-popup').removeClass('open');
+});
+
+$( ".reply-title" ).prependTo( ".comment-respond" );
+// menu button end
+
+// progress bar start
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 500) {     
+        if (!$('#skills').hasClass('active')) {
+            $('#skills').addClass('active');
+            $('.progress-bar').toggleClass('coding');
+            $('.progress-bar.addWordpress').toggleClass('wordpress');
+            $('.progress-bar.addSketch').toggleClass('sketch');
+            $('.progress-bar.addJquery').toggleClass('jQuery');  
+        }
+    } 
+});
+// progress bar end
+
+// return to top start
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 350) {        
+        $("#return-to-top").css("display", "block");  
+    } else {
+        $("#return-to-top").css("display", "none");
+    }
+});
+// return to top end
+
+// counter up number start
+// let CountNumber=0
+// if(CountNumber==0){
+    $('.count').counterUp({
+        delay:10,
+        time:1000,    
+    });
+//     CountNumber=1;
+// }
+// console.log(CountNumber)
+
+// counter up number end 
+
